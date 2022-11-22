eeeb83d9dbf4622902bb439332961555cb128029
thegreenearth2565 <std01@pccpl.ac.th>
2022-11-22 15:49:01 +0700

    a

--- about.php
+++ about.php
@@ -173,14 +173,32 @@
             <div class="row">
                 <div class="col-lg-6" style="min-height: 500px;">
                     <div class="position-relative h-100">
-                        <img class="position-absolute w-100 h-100" src="img/about.jpg" style="object-fit: cover;">
+                        <img class="position-absolute w-100 h-100" src="img/about.png" style="object-fit: cover;">
                     </div>
                 </div>
                 <div class="col-lg-6 pt-5 pb-lg-5">
                     <div class="about-text bg-white p-4 p-lg-5 my-lg-5">
                         <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">About Us</h6>
-                        <h1 class="mb-3">We Provide Best Tour Packages In Your Budget</h1>
-                        <p>Dolores lorem lorem ipsum sit et ipsum. Sadip sea amet diam dolore sed et. Sit rebum labore sit sit ut vero no sit. Et elitr stet dolor sed sit et sed ipsum et kasd ut. Erat duo eos et erat sed diam duo</p>
+                        <h1 class="mb-3">MR.MAHAPHAN CHANTAKHUN</h1>
+                        <p>
+                            <?php
+                            $teaching = "<p>รายวิชาที่สอน<br>
+                            ————————————<br>
+                            ว21103   เทคโนโลยีและวิทยาการคำนวณ 1 <br> 
+                            ว23103   เทคโนโลยีและวิทยาการคำนวณ 3 <br>
+                            ว20206  การออกแบบและเทคโนโลยี <br> </p>";
+                                       
+                           
+                             $address = "ที่อยู่ <br>
+                             ————————————<br>
+                             109/29 ม. 4 (พรหมพิรามซิตี้) บ้านกรับพวงใต้<br>
+                             ต.พรหมพิราม อ.พรหมพิราม จ.พิษณุโลก 65150";   
+                             
+                            echo $teaching;
+                            echo $address;
+
+                            ?>
+                        </p>
                         <div class="row mb-4">
                             <div class="col-6">
                                 <img class="img-fluid" src="img/about-1.jpg" alt="">
@@ -189,7 +207,7 @@
                                 <img class="img-fluid" src="img/about-2.jpg" alt="">
                             </div>
                         </div>
-                        <a href="" class="btn btn-primary mt-1">Book Now</a>
+                        <a href="contact.php" class="btn btn-primary mt-1">Contact Now</a>
                     </div>
                 </div>
             </div>
