b7c3415e276ae29b622d6676a867300e9b7e825f
thegreenearth <juniorprog.com@pccpl.ac.th>
2023-01-23 15:22:52 +0700

    do while

--- whileLoop.php
+++ whileLoop.php
@@ -3,16 +3,17 @@
     <style>
         div {
             align-self: start;
-            width: 40%;
+            width: 30%;
             border: 5px solid green;
-            padding: 5px;
+            padding-left: 20px;
+            padding-top: 20px;
             margin: auto;
         }
         .divIn{
-            width: 35%;
+            width: 30%;
             border: 2px solid red;
-            padding: 3px;
-            margin: auto;
+            padding-left: 30px;
+            margin: 5px;
         }
     </style>
 <body>
@@ -21,12 +22,13 @@
 กรุณากรอกตัวเลข: <input type="text" name="num">
 <input type="submit">
 </form>
-
+<h1>สูตรคูณแม่ <?php echo $_POST["num"]; ?>
 <?php
     $count = 1;
     $num = $_POST["num"];
     settype($num,'int');
     echo "<h2>การทำซ้ำแบบ While Loop:</h2>";
+    echo "<hr>";
     while($count <= 12){
         $sum = $num * $count;
         echo "<div class='divIn'>";
@@ -37,9 +39,25 @@
     }
 ?>
 <?php
+    $count = 1;
+    $num = $_POST["num"];
+    settype($num,'int');
+    echo "<h2>การทำซ้ำแบบ Do While Loop:</h2>";
+    echo "<hr>";
+    do{
+        $sum = $num * $count;
+        echo "<div class='divIn'>";
+        echo $num. "*". $count. " = ". $sum;
+        $count++;
+        echo "<br>";
+        echo "</div>";
+    }while($count <= 12);
+?>
+<?php
     $num = $_POST["num"];
     settype($num,'int');
     echo "<h2>การทำซ้ำแบบ for Loop:</h2>";
+    echo "<hr>";
     for($count=1;$count<=12;$count++){
         $sum = $num * $count;
         echo "<div class='divIn'>";
