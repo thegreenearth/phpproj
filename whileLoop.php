fb6b4767ccdd5705646b8be76b96c6feaf810498
thegreenearth <juniorprog.com@pccpl.ac.th>
2023-01-23 14:42:27 +0700

    alignment

--- whileLoop.php
+++ whileLoop.php
@@ -3,16 +3,16 @@
     <style>
         div {
             align-self: start;
-            width: 50%;
+            width: 40%;
             border: 5px solid green;
             padding: 5px;
-            margin: 10px;
+            margin: auto;
         }
         .divIn{
-            width: 45%;
-            border: 3px solid red;
-            padding: 2px;
-            margin: 5px;
+            width: 35%;
+            border: 2px solid red;
+            padding: 3px;
+            margin: auto;
         }
     </style>
 <body>
@@ -26,11 +26,24 @@
     $count = 1;
     $num = $_POST["num"];
     settype($num,'int');
+    echo "<h2>การทำซ้ำแบบ While Loop:</h2>";
     while($count <= 12){
         $sum = $num * $count;
         echo "<div class='divIn'>";
         echo $num. "*". $count. " = ". $sum;
         $count++;
+        echo "<br>";
+        echo "</div>";
+    }
+?>
+<?php
+    $num = $_POST["num"];
+    settype($num,'int');
+    echo "<h2>การทำซ้ำแบบ for Loop:</h2>";
+    for($count=1;$count<=12;$count++){
+        $sum = $num * $count;
+        echo "<div class='divIn'>";
+        echo $num. "*". $count. " = ". $sum;
         echo "<br>";
         echo "</div>";
     }
